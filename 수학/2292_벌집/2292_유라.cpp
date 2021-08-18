c53d0f4f3eed035eb334fc59e31a17f999f3614b
Yoola Choi <39353959+yoooooola@users.noreply.github.com>
2021-08-18 22:40:36 +0900

    2292번 주석 추가

--- 수학/2292_벌집/2292_유라.cpp
+++ 수학/2292_벌집/2292_유라.cpp
@@ -6,12 +6,12 @@ int main()
     int n, num = 1, row = 1;
     scanf("%d", &n);
     
-    while(1) {
+    while(1) { // to find row
         if (n <= num){
             printf("%d", row);
             break;
         }
-        num += (row * 6);
+        num += (row * 6); // num: total number of nodes from now + row 'n' consists of '6n' nodes.
         row++;
     }
 
