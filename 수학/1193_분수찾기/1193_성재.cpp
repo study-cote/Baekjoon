9575672c602d58ae7e5a05b7803a82e9f5f29ae6
lacoon2874 <33238887+lacoon2874@users.noreply.github.com>
2021-08-18 20:52:47 +0900

    1193 코드 변경

--- 수학/1193_분수찾기/1193_성재.cpp
+++ 수학/1193_분수찾기/1193_성재.cpp
@@ -6,7 +6,7 @@ class findFrac {
 	int data, count, sum;
 
 public:
-	findFrac(int n) : data(n), count(1), sum(0) {   // 생성자
+	findFrac(int n) : data(n), count(1), sum(1) {   // 생성자
 		
 	}
 	void answer();  // 결과값 구하는 함수
@@ -15,16 +15,16 @@ public:
 void findFrac::answer() {  // 결과값 구하는 함수
 	for (;;count++) {      // 몇 번째 대각선인지 구함
 		sum += count;
-		if (data <= sum) {
+		if (data < sum) {
 			break;
 		}
 	}
 
 	if (count % 2) {     // 홀수 번째 대각선일 경우
-		cout << sum - data + 1 << '/' << count - (sum - data);
+		cout << sum - data << '/' << count + 1 - (sum - data);
 	}
 	else {   // 짝수 번째 대각선일 경우
-		cout << count - (sum - data) << '/' << sum - data + 1;
+		cout << count + 1 - (sum - data) << '/' << sum - data;
 	}
 }
 
