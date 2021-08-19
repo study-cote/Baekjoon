e71faa2c5e43c129e66e22504d925abdb597ed2f
lacoon2874 <33238887+lacoon2874@users.noreply.github.com>
2021-08-20 03:20:21 +0900

    1193 변수명, 주석 수정

--- 수학/1193_분수찾기/1193_성재.cpp
+++ 수학/1193_분수찾기/1193_성재.cpp
@@ -3,36 +3,36 @@ using std::cin;
 using std::cout;
 
 class findFrac {
-	int data, count, sum;
+	int x, count, sum;
 
 public:
-	findFrac(int n) : data(n), count(1), sum(1) {   // 생성자
+	findFrac(int n) : x(n), count(1), sum(1) {   // 생성자, n은 입력값
 		
 	}
 	void answer();  // 결과값 구하는 함수
 };
 
 void findFrac::answer() {  // 결과값 구하는 함수
-	for (;;count++) {      // 몇 번째 대각선인지 구함
+	for (;;count++) {   // count와 sum의 값은 생성자에서 1로 초기화 됨
 		sum += count;
-		if (data < sum) {
+		if (x < sum) {
 			break;
 		}
 	}
 
 	if (count % 2) {     // 홀수 번째 대각선일 경우
-		cout << sum - data << '/' << count + 1 - (sum - data);
+		cout << sum - x << '/' << count + 1 - (sum - x);
 	}
 	else {   // 짝수 번째 대각선일 경우
-		cout << count + 1 - (sum - data) << '/' << sum - data;
+		cout << count + 1 - (sum - x) << '/' << sum - x;
 	}
 }
 
 int main(void) {
-	int num;
+	int x;
 
-	cin >> num;
-	findFrac temp(num);
+	cin >> x;
+	findFrac temp(x);
 	temp.answer();
 
 	return 0;
