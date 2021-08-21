cc69ecd5275909484f8df22919ac9956129afec2
lacoon2874 <33238887+lacoon2874@users.noreply.github.com>
2021-08-21 23:29:02 +0900

    10250_성재 주석 추가

--- 수학/10250_ACM_호텔/10250_성재.cpp
+++ 수학/10250_ACM_호텔/10250_성재.cpp
@@ -8,22 +8,22 @@ class hotel {
 	int h, w, n;
 
 public:
-	void inputData();
-	void printAnswer();
+	void inputData();      // 데이터 입력
+	void printAnswer();    // 결과값 계산 및 출력
 };
 
-void hotel::inputData() {
+void hotel::inputData() {   // 데이터 입력
 	cin >> h >> w >> n;
 }
 
-void hotel::printAnswer() {
-	if (n % h) {
+void hotel::printAnswer() {  // 결과값 계산 및 출력
+	if (n % h) {           // 층수 출력
 		cout << n % h;
 	}
 	else {
 		cout << h;
 	}
-	cout.width(2);
+	cout.width(2);     // 호수 출력 시 자리 2칸 확보
 	cout << (int)ceil((double)n / h);
 	cout << '\n';
 }
@@ -33,10 +33,10 @@ int main(void) {
 
 	hotel temp;
 	cin >> T;
-	cout.fill('0');
+	cout.fill('0');     // 호수 출력 시 빈 자리에 0 채워넣음
 	for (int i = 0; i < T; i++) {
-		temp.inputData();
-		temp.printAnswer();
+		temp.inputData();     // h, w, n 입력
+		temp.printAnswer();   // 결과 출력
 	}
 
 	return 0;
