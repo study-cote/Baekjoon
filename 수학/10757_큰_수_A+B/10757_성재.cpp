3326e109d22ff64d44048d85b33273189c0e2ff5
lacoon2874 <33238887+lacoon2874@users.noreply.github.com>
2021-08-22 21:56:44 +0900

    10757 수정 및 주석 추가

--- 수학/10757_큰_수_A+B/10757_성재.cpp
+++ 수학/10757_큰_수_A+B/10757_성재.cpp
@@ -1,52 +1,67 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using std::cin;
 using std::cout;
-using std::ios_base;
 using std::string;
 
-int main(void) {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
+class plus {
+	string a, b, c;
+	int sum, up;
 
-	string a, b, c, d;
-	int sum, up = 0;
+public:
+	plus() : sum(0), up(0) {}      // 생성자
+
+	void inputData();     // 데이터 입력
+	void printAnswer();   // 결과값 계산 및 출력
+};
+
+void plus::inputData() {    // 데이터 입력
 	cin >> a >> b;
-	if (a.length() > b.length()) {
+}
+
+void plus::printAnswer() {             // 결과값 계산 및 출력
+	if (a.length() > b.length()) {       // a, b 중에 더 긴 string의 길이로 c 크기 결정
 		c.reserve(a.length() + 1);
-		d.reserve(a.length() + 1);
 	}
 	else {
 		c.reserve(b.length() + 1);
-		d.reserve(b.length() + 1);
 	}
-	while (!a.empty() && !b.empty()) {
-		sum = a.back() + b.back() + up - 96;
-		up = sum / 10;
-		c.push_back(sum % 10 + 48);
+
+	while (!a.empty() && !b.empty()) {         // a, b의 젤 뒷자리 부터 더하기
+		sum = (a.back() - '0') + (b.back() - '0') + up;   // a + b + 올림수
+		up = sum / 10;     // 올림수
+		c.push_back(sum % 10 + '0');   // 올리고 남은 나머지
 		a.pop_back();
 		b.pop_back();
 	}
-	while (!a.empty()) {
-		sum = a.back() + up - 48;
+	while (!a.empty()) {     // a 앞자리 남았을 경우
+		sum = a.back() + up - '0';
 		up = sum / 10;
-		c.push_back(sum % 10 + 48);
+		c.push_back(sum % 10 + '0');
 		a.pop_back();
 	}
-	while (!b.empty()) {
-		sum = b.back() + up - 48;
+	while (!b.empty()) {     // b 앞자리 남았을 경우
+		sum = b.back() + up - '0';
 		up = sum / 10;
-		c.push_back(sum % 10 + 48);
+		c.push_back(sum % 10 + '0');
 		b.pop_back();
 	}
-	if (up) {
+	if (up) {      // 다 더하고 올림수 남았을 경우
 		c.push_back('1');
 	}
-	while (!c.empty()) {
-		d.push_back(c.back());
-		c.pop_back();
-	}
-	cout << d;
+
+	std::reverse(c.begin(), c.end());    // c가 낮은자리부터 앞에 들어있으므로 문자열 뒤집기
+	cout << c;
+}
+
+int main(void) {
+	cin.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+
+	plus temp;
+	temp.inputData();     // 데이터 입력
+	temp.printAnswer();   // 결과 출력
 
 	return 0;
 }
