5ecb53793b1cf3e62716162493c447365887b060
lacoon2874 <33238887+lacoon2874@users.noreply.github.com>
2021-08-23 01:11:10 +0900

    2839 수정

--- 수학/2839_설탕_배달/2839_성재.cpp
+++ 수학/2839_설탕_배달/2839_성재.cpp
@@ -12,29 +12,24 @@ public:
 };
 
 void sugar::printAnswer() {
-	switch (n) {
-	case 4:          // 3과 5 조합으로 n을 만들 수 없음
-		cout << -1;
+	switch (n % 3) {       // 15a + 3b형태로 변환하기 위해 +@ 값 따로 저장
+	case 1:             // 15a + 10 + 3b
+		n -= 10;
+		count = 2;
 		break;
-	case 7:
-		cout << -1;
+	case 2:             // 15a + 5 + 3b
+		n -= 5;
+		count = 1;
 		break;
-	default:         // 3과 5 조합으로 n 만들어질때
-		switch (n % 3) {       // 15a + 3b형태로 변환하기 위해 +@ 값 따로 저장
-		case 1:             // 15a + 10 + 3b
-			n -= 10;
-			count = 2;
-			break;
-		case 2:             // 15a + 5 + 3b
-			n -= 5;
-			count = 1;
-			break;
-		default:            // 15a + 3b
-			count = 0;
-		}
+	default:            // 15a + 3b
+		count = 0;
+	}
+	if (n < 0) {     // 3과 5의 조합으로 만들 수 없는 수
+		cout << -1;
+	}
+	else {
 		cout << (n / 15) * 3 + (n % 15) / 3 + count;      // 15a + 3b + @
 	}
-	
 }
 
 int main(void) {
