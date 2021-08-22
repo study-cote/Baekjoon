ee764c63d633adbd205d6d02eac37e244b6dc31a
lacoon2874 <33238887+lacoon2874@users.noreply.github.com>
2021-08-22 22:41:30 +0900

    1011 수정 및 주석 추가

--- 수학/1011_Fly_me_to_the_Alpha_Centauri/1011_성재.cpp
+++ 수학/1011_Fly_me_to_the_Alpha_Centauri/1011_성재.cpp
@@ -3,30 +3,46 @@
 using std::cin;
 using std::cout;
 
+class movePlanet {
+	int x, y, dif;
+	int a;
+
+public:
+	movePlanet() {}   // 생성자
+
+	void inputData();      // 데이터 입력
+	void printAnswer();    // 결과값 계산 및 출력
+};
+
+void movePlanet::inputData() {   // 데이터 입력
+	cin >> x >> y;
+	dif = y - x;    // 거리 계산
+}
+
+void movePlanet::printAnswer() {   // 결과값 계산 및 출력
+	a = (int)sqrt(dif);
+	
+	if (dif == a * a) {
+		cout << a * 2 - 1 << '\n';
+	}
+	else if (dif <= a * (a + 1)) {
+		cout << a * 2 << '\n';
+	}
+	else {
+		cout << a * 2 + 1 << '\n';
+	}
+}
+
 int main(void) {
 	cin.tie(NULL);
 	std::ios_base::sync_with_stdio(false);
 
-	int T, x, y;
-	long long int dif, a;
+	int T;
+	movePlanet temp;
 	cin >> T;
 	for (int i = 0; i < T; i++) {
-		cin >> x >> y;
-		dif = y - x;
-
-		a = (long long int)sqrt(dif);
-		if (dif <= a * (a + 1) - a) {
-			cout << a * 2 - 1 << '\n';
-		}
-		else if (dif <= a * (a + 1)) {
-			cout << a * 2 << '\n';
-		}
-		else if (dif <= (a + 1) * (a + 2) - (a + 1)) {
-			cout << (a + 1) * 2 - 1 << '\n';
-		}
-		else {
-			cout << (a + 1) * 2 << '\n';
-		}
+		temp.inputData();         // 데이터 입력
+		temp.printAnswer();       // 결과 출력
 	}
 
 	return 0;
