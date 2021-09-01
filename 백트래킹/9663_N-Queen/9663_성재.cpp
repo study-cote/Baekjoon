674c5994048e575bc7ac63a471f03fb655e617ac
lacoon2874 <33238887+lacoon2874@users.noreply.github.com>
2021-09-01 22:40:18 +0900

    9663번 업로드

---
+++ 백트래킹/9663_N-Queen/9663_성재.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+class nQueen {
+	int n, result, dif;
+	bool is_empty;
+
+	void inputData();         // 데이터 입력
+	void func(bool**, int);   // 백트래킹 함수
+
+public:
+	nQueen() : result(0) {}   // 생성자
+
+	void answer();   // 함수 실행 및 출력
+};
+
+void nQueen::inputData() {   // 데이터 입력
+	cin >> n;
+}
+
+void nQueen::func(bool** arr, int cur) {   // 백트래킹 함수
+	if (cur < n) {   // 검사할 행이 남았을 때
+		for (int i = 0; i < n; i++) {   // 각 열에 퀸이 들어갈 수 있는지 검사
+			is_empty = true;
+			for (int j = 0; j < cur; j++) {   // 현재 행보다 앞에 행에서 퀸이 겹치는지 검사
+				if (arr[j][i]) {        // 같은 열에 있을 경우
+					is_empty = false;
+					break;
+				}
+				dif = cur - j;
+				if (i - dif > -1 && arr[j][i - dif]) {   // 왼쪽 위 대각선에 있을 경우
+					is_empty = false;
+					break;
+				}
+				if (i + dif < n && arr[j][i + dif]) {   // 오른쪽 위 대각선에 있을 경우
+					is_empty = false;
+					break;
+				}
+			}
+			if (is_empty) {    // 세가지 경우 모두 없었을 경우
+				arr[cur][i] = true;   // 퀸 배치
+				func(arr, cur + 1);   // 다음 행 검사
+				arr[cur][i] = false;  // 퀸 제거
+			}
+		}
+	}
+	else {   // 퀸이 n개 놓아졌을 경우
+		result++;
+	}
+}
+
+void nQueen::answer() {
+	inputData();
+
+	bool** arr;
+	arr = new bool*[n];
+	for (int i = 0; i < n; i++) {
+		arr[i] = new bool[n];
+		for (int j = 0; j < n; j++) {
+			arr[i][j] = false;
+		}
+	}
+
+	func(arr, 0);
+
+	cout << result;   // 결과 값 출력
+}
+
+int main(void) {
+	cin.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+
+	nQueen temp;
+	temp.answer();
+
+	return 0;
+}
