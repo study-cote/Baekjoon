4fc66483b1e818b7cda27dff9256d47ea8541578
lacoon2874 <33238887+lacoon2874@users.noreply.github.com>
2021-09-01 22:18:54 +0900

    15651번 업로드

---
+++ 백트래킹/15651_N과 M (3)/15651_성재.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+class progression {
+	int n, m;
+
+	void inputData();       // 데이터 입력
+	void func(int*, int);   // 백트래킹
+
+public:
+	void answer();   // 함수 실행 및 초기화
+};
+
+void progression::inputData() {  // 데이터 입력
+	cin >> n >> m;
+}
+
+void progression::func(int* arr, int cur) {   // 백트래킹
+	if (cur < m) {
+		for (int i = 0; i < n; i++) {    // 가능한 경우의 수에 대해 표기 후 다음 레벨로 내려가 함수 실행
+			arr[cur] = i + 1;
+			func(arr, cur + 1);
+		}
+	}
+	else {   // 출력
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+	}
+}
+
+void progression::answer() {    // 함수 실행 및 초기화
+	inputData();
+	int* arr = new int[m];
+	func(arr, 0);
+}
+
+int main(void) {
+	cin.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+
+	progression temp;
+	temp.answer();
+
+	return 0;
+}
